037889da15b05be97ab17c052a0a0e945353633b
Zawartas <32157216+Zawartas@users.noreply.github.com>
2017-09-21 15:15:14 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int _numberOfQuestions;
 
 int main()
 {
+    srand(time(nullptr));
     do
     {
         cout << "Numbers range? " ;
@@ -19,20 +20,13 @@ int main()
     }
     while (getNumber(_numberOfQuestions) == false);
 
-    if (_numberOfQuestions > _max){
-        cout << "\n\nCan't have more tasks than maximum allowed numbers range. Program terminated.\n\n";
+    if (_numberOfQuestions > _max*_max){
+        cout << "\nCan't have more tasks than "<< _max << "^" <<_max <<". \nProgram terminated.\n\n";
         return 0;
     }
 
-    int *_questions = new int[_numberOfQuestions];
     pair_of_numbers *_questionsSet = new pair_of_numbers[_numberOfQuestions];
-
-
-    make_table_of_questions (_questions, _numberOfQuestions, _max);
     make_set_of_questions (_questionsSet, _numberOfQuestions, _max);
-
-//    for (int i = 0; i < _numberOfQuestions; i++)
-    cout << _questionsSet->x << " " << _questionsSet->y << endl;
 
     do
     {
@@ -43,8 +37,7 @@ int main()
 
     for (int i = 0; i < _numberOfQuestions; i++)
     {
-
-        thread first (question, _max, *(_questions + i), ref(_timeOut), ref(_calcDone), ref(_threadOneFinished));
+        thread first (question, i, _questionsSet, ref(_timeOut), ref(_calcDone), ref(_threadOneFinished));
         thread second (wait, _time, ref(_timeOut), ref(_calcDone), ref(_threadOneFinished));
 
         first.join();
